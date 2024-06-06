3efa3b0c9f8025c7f44a4ca79b0fed8acf70657b
knyaz-ostrov <72760787+knyaz-ostrov@users.noreply.github.com>
2024-06-06 23:23:47 +0500

    Рефакторинг

--- scripts/agregator.py
+++ scripts/agregator.py
@@ -7,60 +7,77 @@ from database.mongodb import GetData
 
 class Agregator:
     def __init__(self, input_data: dict) -> None:
-        self.input_data = input_data
-        self.dt_from = self.__convert_to_datetime(input_data['dt_from'])
-        self.dt_upto = self.__convert_to_datetime(input_data['dt_upto'])
+        self.__input_data = input_data
+        self.__time_format = "%Y-%m-%dT%H:%M:%S"
 
-        self.data = GetData(self.dt_from, self.dt_upto).result
+        self.__dt_from = datetime.strptime(input_data['dt_from'], self.__time_format)
+        self.__dt_upto = datetime.strptime(input_data['dt_upto'], self.__time_format)
+        self.__group_type = self.__input_data['group_type']
+
+        self.__data = GetData(self.__dt_from, self.__dt_upto).result
 
         self.output_data = {
             'dataset': [0],
-            'labels': [self.dt_from]
+            'labels': [self.__dt_from]
         }
-        
+
         self.__make_labels()
         self.__agregating()
-    
-    def __convert_to_datetime(self, date: str) -> datetime:
-        return datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
+        self.__convert_datetime_to_string()
+
+
 
     def __make_labels(self) -> None:
-        '''Составление всех временных промежутков'''
-        start_date_types = {
-            'month': [self.dt_from.year, self.dt_from.month, 1],
-            'day':   [self.dt_from.year, self.dt_from.month, self.dt_from.day],
-            'hour':  [self.dt_from.year, self.dt_from.month, self.dt_from.day, self.dt_from.hour]
+        '''Занесение всех временных промежутков в output_data'''
+
+        # Определение базовой даты, от которой пойдет занесение дат
+        date = datetime(
+            *{
+            'month': [self.__dt_from.year, self.__dt_from.month, 1],
+            'day':   [self.__dt_from.year, self.__dt_from.month, self.__dt_from.day],
+            'hour':  [self.__dt_from.year, self.__dt_from.month, self.__dt_from.day, self.__dt_from.hour]
+            }[self.__group_type]
+        )
+
+        # Словарь для определения крайней даты, на которой нужно остановить процесс
+        end_date_types = {
+            'month': [self.__dt_upto.year, self.__dt_upto.month, 1],
+            'day':   [self.__dt_upto.year, self.__dt_upto.month, self.__dt_upto.day],
+            'hour':  [self.__dt_upto.year, self.__dt_upto.month, self.__dt_upto.day, self.__dt_upto.hour]
         }
-        date = datetime(*start_date_types[self.input_data['group_type']])
+
+        # Словарь для определения шага отсчета дат
         relativedelta_types = {
             'month': relativedelta(months=1),
             'day':   relativedelta(days=1),
             'hour':  relativedelta(hours=1)
         }
-        end_date_types = {
-            'month': [self.dt_upto.year, self.dt_upto.month, 1],
-            'day':   [self.dt_upto.year, self.dt_upto.month, self.dt_upto.day],
-            'hour':  [self.dt_upto.year, self.dt_upto.month, self.dt_upto.day, self.dt_upto.hour]
-        }
+
         while True:
-            if date >= datetime(*end_date_types[self.input_data['group_type']]):
+            if date >= datetime(*end_date_types[self.__group_type]):
                 break
-            date += relativedelta_types[self.input_data['group_type']]
+            date += relativedelta_types[self.__group_type]
             self.output_data['labels'].append(date)
             self.output_data['dataset'].append(0)
 
+
+
     def __agregating(self) -> None:
         '''Агрегация полученных данных из бд'''
-        for obj in self.data:
+
+        for obj in self.__data:
             for i, dt in enumerate(self.output_data['labels'], 0):
-                condition_dict = {
+                # Проверяет не выходит ли объект из бд за временные рамки даты из labels
+                if {
                     'month': obj['dt'].year != dt.year or obj['dt'].month != dt.month,
                     'day'  : obj['dt'].year != dt.year or obj['dt'].month != dt.month or obj['dt'].day != dt.day,
                     'hour' : obj['dt'].year != dt.year or obj['dt'].month != dt.month or obj['dt'].day != dt.day or obj['dt'].hour != dt.hour
-                }
-                if condition_dict[self.input_data['group_type']]:
-                    continue
+                }[self.__group_type]: continue
+
                 self.output_data['dataset'][i] += obj['value']
-        
+
+
+
+    def __convert_datetime_to_string(self):
         for i in range(len(self.output_data['labels'])):
-            self.output_data['labels'][i] = self.output_data['labels'][i].strftime("%Y-%m-%dT%H:%M:%S")
+            self.output_data['labels'][i] = self.output_data['labels'][i].strftime(self.__time_format)
