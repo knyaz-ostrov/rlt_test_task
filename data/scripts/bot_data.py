ef055ec76bfd1bd9a40791e2b72882bd2a60a031
knyaz-ostrov <72760787+knyaz-ostrov@users.noreply.github.com>
2024-06-10 18:04:02 +0500

    Скрыл ненужные переменные в классе BotMessageText; Добавил комментарии.

--- data/scripts/bot_data.py
+++ data/scripts/bot_data.py
@@ -1,5 +1,7 @@
+"""
+Модуль для текста, который используется в функционале бота
+"""
 import lxml.etree as ET
-
 
 
 FILE_PATH = 'data/data/bot_data.xml'
@@ -7,13 +9,13 @@ path_template = './/{}'
 filter = '[@id="{}"]'
 
 
-
 class BotMessageText:
-    bot_data = ET.parse(FILE_PATH)
-    category = 'text'
-    path = path_template.format(category) + filter
+    """
+    Класс для хранения текстовых сообщений отправляемых ботом
+    """
+    __bot_data = ET.parse(FILE_PATH)
+    __category = 'text'
+    __path = path_template.format(__category) + filter
 
-    start             = bot_data.xpath(path.format('start'))[0].text
-    incorrect_request = bot_data.xpath(path.format('incorrect_request'))[0].text
-
-    del bot_data, category, path
+    start             = __bot_data.xpath(__path.format('start'))[0].text
+    incorrect_request = __bot_data.xpath(__path.format('incorrect_request'))[0].text
