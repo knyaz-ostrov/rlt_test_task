c8e37234570185abe21e9b6e4ce470f7014f86d7
knyaz-ostrov <72760787+knyaz-ostrov@users.noreply.github.com>
2024-06-10 20:55:03 +0500

    Рефакторинг

--- data/scripts/bot_data.py
+++ data/scripts/bot_data.py
@@ -5,8 +5,8 @@ import lxml.etree as ET
 
 
 FILE_PATH = 'data/data/bot_data.xml'
-path_template = './/{}'
-filter = '[@id="{}"]'
+XPATH_TEMPLATE = './/{}'
+XPATH_FILTER = '[@id="{}"]'
 
 
 class BotMessageText:
@@ -15,7 +15,7 @@ class BotMessageText:
     """
     __bot_data = ET.parse(FILE_PATH)
     __category = 'text'
-    __path = path_template.format(__category) + filter
+    __path = XPATH_TEMPLATE.format(__category) + XPATH_FILTER
 
     start             = __bot_data.xpath(__path.format('start'))[0].text
     incorrect_request = __bot_data.xpath(__path.format('incorrect_request'))[0].text
