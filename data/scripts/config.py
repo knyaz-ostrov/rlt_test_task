5627af48c43132d48b4515c328c35f0b7eb14b50
knyaz-ostrov <72760787+knyaz-ostrov@users.noreply.github.com>
2024-06-10 18:06:21 +0500

    Добавил комментарии

--- data/scripts/config.py
+++ data/scripts/config.py
@@ -1,20 +1,29 @@
+"""
+Модуль для извлечения конфигурационных файлов
+из json-файла.
+"""
 import json
-
 
 
 FILE_PATH = 'data/data/config.json'
 
 
-
 class BotConfig:
+    """
+    Класс для хранения конфигурационных
+    данных бота
+    """
     with open(FILE_PATH) as file:
         __bot_config = json.load(file)['bot']
 
     token = __bot_config['token']
 
 
-
 class MongoConfig:
+    """
+    Класс для хранения конфигурационных данных
+    MongoDB
+    """
     with open(FILE_PATH) as file:
         __mongo_config = json.load(file)['mongodb']
 
